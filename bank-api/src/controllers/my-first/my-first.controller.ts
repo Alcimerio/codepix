e7f6c8b9da01e3d5ea0180d76b623381aebe7ddb
Alcimerio Rangel <alcimerioj@id.uff.br>
2021-02-07 22:04:30 -0300

    add nest-api project

---
+++ bank-api/src/controllers/my-first/my-first.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('my-first')
+export class MyFirstController {
+
+  @Get('hello-world')
+  index() {
+    return {
+      'key': 'value'
+    }
+  }
+}
